879e181dab29037fa2db80cda1bb58a4f1ce63d7
mirrorhanyu <mirrorhanyu@gamil.com>
2019-04-19 22:28:18 +0800

    return with status code and headers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from http import HTTPStatus
 import requests
 
 # An instance of this Flask class will be our WSGI application.
@@ -9,6 +10,8 @@ DEVELOPERS = '/developers'
 LANGUAGES = '/languages'
 TRENDING_URL = 'https://github-trending-api.now.sh'
 
+HEADER = {'Content-Type': 'application/json'}
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -16,16 +19,19 @@ def hello():
 @app.route(REPOSITORIES)
 def repository():
     params = request.url.split(REPOSITORIES)[1]
-    repositories = requests.get(TRENDING_URL + REPOSITORIES + params).json()
-    return str(repositories)
+    repositories = requests.get(TRENDING_URL + REPOSITORIES + params)
+    return repositories.text, HTTPStatus.OK, HEADER
 
 @app.route(DEVELOPERS)
 def developer():
     params = request.url.split(DEVELOPERS)[1]
-    developers = requests.get(TRENDING_URL + DEVELOPERS + params).json()
-    return str(developers)
+    developers = requests.get(TRENDING_URL + DEVELOPERS + params)
+    return developers.text, HTTPStatus.OK, HEADER
 
 @app.route(LANGUAGES)
 def language():
-    languages = requests.get(TRENDING_URL + LANGUAGES).json()
-    return str(languages)
+    languages = requests.get(TRENDING_URL + LANGUAGES)
+    return languages.text, HTTPStatus.OK, HEADER
+
+if __name__ == '__main__':
+    app.run()
