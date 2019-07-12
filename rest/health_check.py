15dbe0cf11b5225d8b14c356e96d1af2e0e65ee0
mirrorhanyu <mirrorhanyu@gamil.com>
2019-07-12 22:26:56 +0800

    modify url for health check

--- rest/health_check.py
+++ rest/health_check.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 health_check_api = Blueprint('health_check_api', __name__)
 
 
-@health_check_api.route('/health_check')
+@health_check_api.route('/health-check')
 def health_check():
     return "ok"
