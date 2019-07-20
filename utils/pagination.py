63379cc56af24c5dc300e7e7fe0eda03351c36c8
mirrorhanyu <mirrorhanyu@gamil.com>
2019-07-20 20:45:22 +0800

    fix when max pagination is not in the header

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,5 +1,5 @@
 import re
 
 
-def get_max_page(link_header):
-    return max(re.findall(r'page=([0-9]+)>', link_header), key=int) if link_header else 0
+def get_max_page(current_page, link_header):
+    return max(re.findall(r'page=([0-9]+)>', link_header) + [current_page], key=int) if link_header else 0
